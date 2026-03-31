63f0e2ff37635e8e8f1a4712e1369ce1299c23dc
lollocenne <lorenzo.cennerilli@gmail.com>
2026-03-31 21:05:47 +0200

    Change activation function handling

--- main.c
+++ main.c
@@ -23,23 +23,25 @@ typedef enum {
 f64* initializeNeurons(u32 size);
 f64** initializeWeights(u32 currLayer, u32 nextLayer);
 f64* initializeBias(u32 size);
-function* initializeFuncions(ActivationFunction* functionsName, u32 numLayers);
-function* initializeDerivatives(ActivationFunction* functionsName, u32 numLayers);
+function getFunction(ActivationFunction functionName);
+function getFunctionDerivate(ActivationFunction functionName);
 
 typedef struct {
     f64* neurons;
     f64** weights;
     f64* bias;
+    function actFunction;
+    function derActFunction;
 } Layer;
 
-Layer* initializeNetwork(u32* sizes, u32 numLayers);
+Layer* initializeNetwork(u32* sizes, u32 numLayers, ActivationFunction* functionsName);
 
 void freeNetwork(Layer* network, u32* sizes, u32 numLayers);
 
 int main() {
     u32 sizes[NUM_LAYERS] = SIZES;
     ActivationFunction functions[NUM_LAYERS] = FUNCTIONS;
-    Layer* model = initializeNetwork(sizes, NUM_LAYERS);
+    Layer* model = initializeNetwork(sizes, NUM_LAYERS, functions);
     
     freeNetwork(model, sizes, NUM_LAYERS);
     
@@ -70,43 +72,33 @@ f64* initializeBias(u32 size) {
     return (f64*)calloc(size, sizeof(f64));
 }
 
-// Inizialize the array of function pointers for the activation functions
-function* initializeFuncions(ActivationFunction* functionsName, u32 numLayers) {
-    function* functions = (function*)malloc(numLayers * sizeof(function));
-    
-    for (u32 i = 0; i < numLayers; i++) {
-        switch (functionsName[i]){
-            case IDENTITY:    functions[i] = identity;   break;
-            case BINARY_STEP: functions[i] = binaryStep; break;
-            case SIGMOID:     functions[i] = sigmoid;    break;
-            case TANH:        functions[i] = tanh;       break;
-            case RELU:        functions[i] = relu;       break;
-            default:          functions[i] = NULL;       break;
-        }
+// Return an activation function pointer based on the enum value
+function getFunction(ActivationFunction functionName) {
+    switch (functionName){
+        case IDENTITY:    return identity;   break;
+        case BINARY_STEP: return binaryStep; break;
+        case SIGMOID:     return sigmoid;    break;
+        case TANH:        return tanh;       break;
+        case RELU:        return relu;       break;
+        default:          return NULL;       break;
     }
-    
-    return functions;
+    return NULL;
 }
 
-// Inizialize the array of function pointers for the activation functions derivatives
-function* initializeDerivatives(ActivationFunction* functionsName, u32 numLayers) {
-    function* functions = (function*)malloc(numLayers * sizeof(function));
-    
-    for (u32 i = 0; i < numLayers; i++) {
-        switch (functionsName[i]){
-            case IDENTITY:    functions[i] = derivativeIdentity;   break;
-            case BINARY_STEP: functions[i] = derivativeBinaryStep; break;
-            case SIGMOID:     functions[i] = derivativeSigmoid;    break;
-            case TANH:        functions[i] = derivativeTanh;       break;
-            case RELU:        functions[i] = derivativeRelu;       break;
-            default:          functions[i] = NULL;                 break;
-        }
+// Return an activation function derivative pointer based on the enum value
+function getFunctionDerivate(ActivationFunction functionName) {
+    switch (functionName){
+        case IDENTITY:    return derivativeIdentity;   break;
+        case BINARY_STEP: return derivativeBinaryStep; break;
+        case SIGMOID:     return derivativeSigmoid;    break;
+        case TANH:        return derivativeTanh;       break;
+        case RELU:        return derivativeRelu;       break;
+        default:          return NULL;                 break;
     }
-    
-    return functions;
+    return NULL;
 }
 
-Layer* initializeNetwork(u32* sizes, u32 numLayers) {
+Layer* initializeNetwork(u32* sizes, u32 numLayers, ActivationFunction* functionsName) {
     Layer* network = (Layer*)malloc(numLayers * sizeof(Layer));
     
     for (u32 i = 0; i < numLayers; i++) {
@@ -121,6 +113,8 @@ Layer* initializeNetwork(u32* sizes, u32 numLayers) {
         } else {
             network[i].bias = NULL; // No bias for the input layer
         }
+        network[i].actFunction = getFunction(functionsName[i]);
+        network[i].derActFunction = getFunctionDerivate(functionsName[i]);
     }
     
     return network;
