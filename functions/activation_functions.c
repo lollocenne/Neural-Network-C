ec707c5403953df9387895db1c039cb8d9f4b8d7
lollocenne <lorenzo.cennerilli@gmail.com>
2026-04-16 22:36:38 +0200

    Added activation function

--- functions/activation_functions.c
+++ functions/activation_functions.c
@@ -9,6 +9,8 @@ f64 tanh(f64 x)                 {f64 exp1 = exp(x), exp2 = 1/exp1; return (exp1 
 f64 relu(f64 x)                 {return x > 0 ? x : 0;}
 f64 leakyRelu(f64 x)            {return x > 0 ? x : 0.01 * x;}
 f64 softPlus(f64 x)             {return log(1 + exp(x));}
+f64 gaussian(f64 x)             {return exp(-x * x);}
+f64 sinusoid(f64 x)             {return sin(x);}
 
 f64 derivativeIdentity(f64 x)   {return 1;}
 f64 derivativeBinaryStep(f64 x) {return 0;}
@@ -17,3 +19,5 @@ f64 derivativeTanh(f64 x)       {f64 t = tanh(x); return 1 - t * t;}
 f64 derivativeRelu(f64 x)       {return x > 0 ? 1 : 0;}
 f64 derivativeLeakyRelu(f64 x)  {return x > 0 ? 1 : 0.01;}
 f64 derivativeSoftPlus(f64 x)   {return 1/(1 + exp(-x));}
+f64 derivativeGaussian(f64 x)   {return -2*x*exp(-x * x);}
+f64 derivativeSinusoid(f64 x)   {return cos(x);}
