554cddc1053df6d2710a735f28569cdc59c7749a
lollocenne <lorenzo.cennerilli@gmail.com>
2026-04-16 22:39:03 +0200

    .

--- functions/activation_functions.h
+++ functions/activation_functions.h
@@ -9,6 +9,9 @@ f64 sigmoid(f64 x);
 f64 tanh(f64 x);
 f64 relu(f64 x);
 f64 leakyRelu(f64 x);
+f64 softPlus(f64 x);
+f64 gaussian(f64 x);
+f64 sinusoid(f64 x);
 
 f64 derivativeIdentity(f64 x);
 f64 derivativeBinaryStep(f64 x);
@@ -16,5 +19,8 @@ f64 derivativeSigmoid(f64 x);
 f64 derivativeTanh(f64 x);
 f64 derivativeRelu(f64 x);
 f64 derivativeLeakyRelu(f64 x);
+f64 derivativeSoftPlus(f64 x);
+f64 derivativeGaussian(f64 x);
+f64 derivativeSinusoid(f64 x);
 
 #endif
